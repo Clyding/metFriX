2a459c9a1689baca43ad3382f99a0ee4d9275fcf
Clyding <105178839+Clyding@users.noreply.github.com>
2024-11-16 19:25:53 -0500

    Upscale database server

---
+++ scripts/connect.js
@@ -0,0 +1,21 @@
+let mysql = require(mysql)
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
+
+connection.connect((err) => {
+    if (err) return console.error(err.message);
+  
+    console.log('Connected to the MySQL server.');
+});
+  
+connection.end((err) => {
+    if (err) return console.error(err.message);
+  
+    console.log('Close the database connection.');
+});
